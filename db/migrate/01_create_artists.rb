bd0c24173f05d540c8590ff10572f2cd3b915481
drbarq <J.Tustin@gmail.com>
2019-05-06 19:01:59 -0600

    Done.

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,2 +1,11 @@
 class CreateArtists < ActiveRecord::Migration[4.2]
+  def change
+    create_table :artists do |t|
+      t.string :name
+    end
+  end
 end
+
+#very weird error, made a type initialy,(Artist over Artists) Migrated the table, then caught my mistake on this method.
+# Ws unable to roll back the 'artist' table with my corected method.  I needed to "fix" this method with the same typo
+# in order to full roll back
